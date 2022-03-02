7f820ed023cb21f307108531050d3e9ea0da20a8
GediminasGricius <g.gricius@ituostas.lt>
2022-03-02 19:00:15 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { NewComponent } from './components/new/new.component';
+
+const appRoutes:Routes=[
+  { path:'', component:ListComponent },
+  { path:'new', component:NewComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    NewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
